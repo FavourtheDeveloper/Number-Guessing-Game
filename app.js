65a9113c7124665aa91872c507dd71d5c1789ea1
FavourtheDev <favourthedeveloper@gmail.com>
2023-07-19 20:19:42 +0100

    Added more feature

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ var displayDigit = document.getElementById("displaydigit");
 var userVal = document.getElementById("userval");
 var reply = document.getElementById("reply");
 var button = document.getElementById("butt");
-var randomNumber = [1, 2, 3, 4, 5];
+var randomNumber = [0, 1, 2, 3, 4, 5];
+
 button.addEventListener('click', function() {
-    var randomNum = Math.round(Math.random() * 4);
+    var randomNum = Math.round(Math.random() * 5);
     var finalRound = randomNumber[randomNum];
     displayDigit.innerHTML = finalRound;
+
     if (userVal.value === "") {
-        alert("Please Guess a Number");
+        alert("Please Guess a Number from 0 - 5");
     } else
     if(userVal.value > 5){
-        alert("Input a number from 1 - 5");
+        alert("Input a number from 0 - 5");
         displayDigit.innerHTML = "";
     } else
     if(userVal.value == finalRound) {
